e5e7bfaac9ade2031a6a265d972b361a835b44ec
gabrielribeirof <dbielribeiro@gmail.com>
2020-04-21 20:31:54 -0300

    chore(core): implement cors in the index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 
 const app = express();
 const server = http.Server(app);
 
+app.use(cors());
 app.use(express.json());
 
 server.listen(3333);
