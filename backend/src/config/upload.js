102f5c81d530ea0627b84c6dba9de1fbe6eb1460
gabrielribeirof <dbielribeiro@gmail.com>
2020-04-21 20:57:25 -0300

    feat: add image upload file

---
+++ backend/src/config/upload.js
@@ -0,0 +1,36 @@
+const multer = require('multer');
+const crypto = require('crypto');
+const { resolve, extname } = require('path');
+
+module.exports = {
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'tmp', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) cb(err);
+
+        const ext = extname(file.originalname);
+
+        cb(null, `${buf.toString('hex')}${ext}`);
+      });
+    }
+  }),
+  limits: {
+    fileSize: 10 * 1024 * 1024
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'images/jpeg',
+      'image/png',
+      'image/gif'
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type'));
+    }
+  }
+} 
