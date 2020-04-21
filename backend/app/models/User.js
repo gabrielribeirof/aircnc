2aeb46d8875817d8b4c44177bb4041fcc37890ca
Gabriel Ribeiro <dbielribeiro@gmail.com>
2020-04-21 08:18:39 +0000

    feat: create user model

---
+++ backend/app/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', UserSchema);
