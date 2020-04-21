a5126a643bc7639ca0feb66385acf00a3d18f8ef
Gabriel Ribeiro <dbielribeiro@gmail.com>
2020-04-21 08:20:01 +0000

    feat: create booking model

---
+++ backend/app/models/Booking.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const BookingSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  approved: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  spot: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Spot'
+  }
+});
+
+module.exports = mongoose.model('Booking', BookingSchema);
