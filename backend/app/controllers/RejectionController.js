294371bc9e6035def63b4261bcb291693183213d
gabrielribeirof <dbielribeiro@gmail.com>
2020-04-21 18:39:38 -0300

    feat: create rejection controller

---
+++ backend/app/controllers/RejectionController.js
@@ -0,0 +1,21 @@
+const Booking = require('../models/Booking');
+
+module.exports = {
+  async store(req, res) {
+    const { booking_id } = req.params;
+
+    try {
+      const booking = await Booking.findById(booking_id).populate('spot');
+
+      if (!booking)
+        return res.status(400).send({ error: 'Booking not found' });
+
+      booking.approved = false;
+      await booking.save();
+
+      return res.send(booking);
+    } catch (err) {
+      return res.status(400).send({ error: 'Error rejecting booking' });
+    }
+  }
+};
