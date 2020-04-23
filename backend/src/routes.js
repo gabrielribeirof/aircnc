9c7ce2baddfb730495599c6deb98a810fe71d438
gabrielribeirof <dbielribeiro@gmail.com>
2020-04-23 17:50:43 -0300

    feat(routes): change the multer configs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const multer = require('multer');
 
-const uploadConfig = require('./app/middlewares/upload');
+const uploadConfig = require('./config/multer');
+
+const AuthMiddleware = require('./app/middlewares/auth');
 
 const SessionController = require('./app/controllers/SessionController');
 const UserController = require('./app/controllers/UserController');
@@ -13,7 +15,9 @@ const RejectionController = require('./app/controllers/RejectionController');
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.post('/sessions', SessionController.store);
+routes.post('/login', SessionController.store);
+
+routes.use(AuthMiddleware);
 
 routes.get('/users', UserController.index);
 routes.get('/users/:user_id', UserController.show);
@@ -23,8 +27,8 @@ routes.delete('/users/:user_id', UserController.delete);
 
 routes.get('/spots', SpotController.index);
 routes.get('/spots/:spot_id', SpotController.show);
-routes.post('/spots', upload.single('thumbnail'), SpotController.store);
-routes.put('/spots/:spot_id', upload.single('thumbnail'), SpotController.update);
+routes.post('/spots', upload.single('file'), SpotController.store);
+routes.put('/spots/:spot_id', upload.single('file'), SpotController.update);
 routes.delete('/spots/:spot_id', SpotController.delete);
 
 routes.get('/bookings', BookingController.index);
