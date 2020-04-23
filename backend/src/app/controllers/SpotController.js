d0ca9d3fe08e84a4b60d5a133c7b580c95d277da
gabrielribeirof <dbielribeiro@gmail.com>
2020-04-23 17:54:30 -0300

    feat(SpotController): implement file model

--- backend/src/app/controllers/SpotController.js
+++ backend/src/app/controllers/SpotController.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const Spot = require('../models/Spot');
 const User = require('../models/User');
+const File = require('../models/File');
 
 module.exports = {
   async index(req, res) {
@@ -31,6 +32,7 @@ module.exports = {
 
   async store(req, res) {
     const { name, price, tags } = req.body;
+    const { originalname: fileoriginalname, filename: filekey } = req.file;
     const { user_id } = req.headers;
 
     try {
@@ -40,14 +42,17 @@ module.exports = {
         fs.unlinkSync(req.file.path);
         return res.status(400).send({ error: 'User not found' });
       }
-      
-      const { filename } = req.file;
+
+      const file = await File.create({
+        name: fileoriginalname,
+        key: filekey
+      });
 
       const spot = await Spot.create({
         user: user_id,
         name,
         price,
-        thumbnail: filename,
+        thumbnail: file.id,
         tags: tags.split(',').map(tag => tag.trim())
       });
 
@@ -69,22 +74,30 @@ module.exports = {
     const { spot_id } = req.params;
 
     try {
-      const spot = await Spot.findById(spot_id);
+      const spot = await Spot.findById(spot_id).populate('thumbnail');
 
       if (!spot)
         return res.status(400).send({ error: 'Spot not found' });
 
       if (req.file) {
-        fs.unlinkSync(`/workspace/aircnc/backend/tmp/uploads/${spot.thumbnail}`);
-        const { filename } = req.file;
+        fs.unlinkSync(`/workspace/aircnc/backend/tmp/uploads/${spot.thumbnail.key}`);
+        
+        await File.findByIdAndDelete(spot.thumbnail.id);
       } else {
-        const filename = spot.thumbnail;
+        const filename = spot.thumbnail.id;
       }
+
+      const { originalname: fileoriginalname, filename: filekey } = req.file;
+
+      const newFile = await File.create({
+        name: fileoriginalname,
+        key: filekey
+      });
       
       await spot.updateOne({
         name,
         price,
-        thumbnail: filename,
+        thumbnail: newFile.id,
         tags: tags.split(',').map(tag => tag.trim())
       });
 
