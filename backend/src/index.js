bc908904bf9b088a1aa7a84cf20adda46971ac9a
gabrielribeirof <dbielribeiro@gmail.com>
2020-04-23 17:51:59 -0300

    feat(core): add files route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const { resolve } = require('path');
 
 const routes = require('./routes');
 
 const app = express();
 const server = http.Server(app);
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(routes);
+
+app.use('/files', express.static(resolve(__dirname, "..", "tmp", "uploads")))
 
 server.listen(3333);
