2f9c239669f73e3e8f3e0669d5d1b86908ea6a2c
gabrielribeirof <dbielribeiro@gmail.com>
2020-04-25 17:52:42 -0300

    chore(core): add mongo connect

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const http = require('http');
+const morgan = require('morgan');
 const { resolve } = require('path');
-
-const routes = require('./routes');
 
 const app = express();
 const server = http.Server(app);
 
-app.use(express.json());
+mongoose.connect(
+  'mongodb+srv://access:TgPuvSxTfJunpAXT@maincluster-jtmlc.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
+
 app.use(cors());
-app.use(routes);
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(require('./routes'));
 
 app.use('/files', express.static(resolve(__dirname, "..", "tmp", "uploads")))
 
