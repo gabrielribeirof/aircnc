d0713a9a2f4f5007018333d831c50882734c8940
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-27 23:53:28 -0300

    feat(routes): pass to typescript

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,16 +1,16 @@
-const { Router } = require('express');
-const multer = require('multer');
+import { Router } from 'express';
+import multer from 'multer';
 
-const uploadConfig = require('./config/multer');
+import uploadConfig from './config/multer';
 
-const AuthMiddleware = require('./app/middlewares/auth');
+import AuthMiddleware from './middlewares/AuthMiddleware';
 
-const SessionController = require('./app/controllers/SessionController');
-const UserController = require('./app/controllers/UserController');
-const SpotController = require('./app/controllers/SpotController');
-const BookingController = require('./app/controllers/BookingController');
-const ApprovalController = require('./app/controllers/ApprovalController');
-const RejectionController = require('./app/controllers/RejectionController');
+import SessionController from './controllers/SessionController';
+import UserController from './controllers/UserController';
+import SpotController from './controllers/SpotController';
+import BookingController from './controllers/BookingController';
+import ApprovalController from './controllers/ApprovalController';
+import RejectionController from './controllers/RejectionController';
 
 const routes = Router();
 const upload = multer(uploadConfig);
@@ -22,13 +22,11 @@ routes.use(AuthMiddleware);
 
 routes.get('/users', UserController.index);
 routes.get('/users/:user_id', UserController.show);
-routes.put('/users/:user_id', UserController.update);
 routes.delete('/users/:user_id', UserController.delete);
 
 routes.get('/spots', SpotController.index);
 routes.get('/spots/:spot_id', SpotController.show);
 routes.post('/spots', upload.single('file'), SpotController.store);
-routes.put('/spots/:spot_id', upload.single('file'), SpotController.update);
 routes.delete('/spots/:spot_id', SpotController.delete);
 
 routes.get('/bookings', BookingController.index);
@@ -39,4 +37,4 @@ routes.delete('/bookings/:booking_id', BookingController.delete);
 routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
 routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
-module.exports = routes;
+export default routes;
