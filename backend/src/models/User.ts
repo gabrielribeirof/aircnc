fc39e93390b081bdeb27c92b520ef43f365dff28
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-27 23:55:20 -0300

    feat(User): pass to typescript

---
+++ backend/src/models/User.ts
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+import bcryptjs from 'bcryptjs';
+
+export interface IUser extends mongoose.Document {
+  name: string,
+  email: string,
+  password: string,
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  spots: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Spot',
+    required: true,
+  }],
+  bookings: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Booking',
+    required: true,
+  }],
+}, {
+  timestamps: true,
+});
+
+// eslint-disable-next-line func-names
+UserSchema.pre<IUser>('save', async function (next) {
+  const hash = await bcryptjs.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+export default mongoose.model<IUser>('User', UserSchema);
