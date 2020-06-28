95548cb42029526fec0ca21a515d88d1bd7d4764
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-27 23:58:03 -0300

    feat(BookingControler): pass to typescript

---
+++ backend/src/controllers/BookingController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from 'express';
+
+import Booking from '../models/Booking';
+import User from '../models/User';
+import Spot from '../models/Spot';
+
+class BookingController {
+  async index(request: Request, response: Response) {
+    try {
+      const bookings = await Booking.find();
+
+      return response.send(bookings);
+    } catch (err) {
+      return response.status(400).send({ error: 'Error loading bookings' });
+    }
+  }
+
+  async show(request: Request, response: Response) {
+    const { booking_id } = request.params;
+
+    try {
+      const booking = await Booking.findById(booking_id);
+
+      if (!booking) {
+        return response.status(400).send({ error: 'Booking not found' });
+      }
+
+      return response.send(booking);
+    } catch (err) {
+      return response.status(400).send({ error: 'Error finding booking' });
+    }
+  }
+
+  async store(request: Request, response: Response) {
+    const { date, user, spot } = request.body;
+
+    try {
+      if (!await User.findById(user)) {
+        return response.status(400).send({ error: 'User not found' });
+      }
+
+      if (!await Spot.findById(spot)) {
+        return response.status(400).send({ error: 'Spot not found' });
+      }
+
+      const nativeDate = new Date(date);
+
+      if (Object.prototype.toString.call(nativeDate) === '[object Date]') {
+        return response.status(400).send({ error: 'Poorly formatted date' });
+      }
+
+      const booking = await Booking.create({
+        date: nativeDate,
+        user,
+        spot,
+      });
+
+      return response.send(booking);
+    } catch (err) {
+      return response.status(400).send({ error: 'Error adding booking' });
+    }
+  }
+
+  async delete(request: Request, response: Response) {
+    const { booking_id } = request.params;
+
+    try {
+      const booking = await Booking.findByIdAndDelete(booking_id);
+
+      if (!booking) {
+        return response.status(400).send({ error: 'Booking not found' });
+      }
+
+      return response.send(booking);
+    } catch (err) {
+      return response.status(400).send({ error: 'Error deleting booking' });
+    }
+  }
+}
+
+export default new BookingController();
