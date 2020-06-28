4a1de64fde1d6789d5931daf4237f02ec72ac0b9
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-28 20:25:24 -0300

    chore(express): change userid type

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,9 +1,10 @@
 import { Express, Request } from 'express';
+import mongoose from 'mongoose';
 
 declare global {
   namespace Express {
     export interface Request {
-      userID: String
+      userID: mongoose.Schema.Types.ObjectId
     }
   }
 }
