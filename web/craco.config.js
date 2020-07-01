7d818e098b95aa1a4ef730821f8f3e68719c67d5
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-30 23:17:15 -0300

    chore(craco): create craco config file

---
+++ web/craco.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
+};
