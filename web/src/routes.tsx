1d69e5ef8a8d60e98d7de495721a8cce0c44df45
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-07-04 23:56:20 -0300

    feat(routes): add routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Home from '@pages/Home';
+import SignUp from '@pages/SignUp';
+import Spots from '@pages/Spots';
+import Details from '@pages/Details';
 
-const Routes = () => (
+const Routes: React.FC = () => (
   <BrowserRouter>
     <Route component={Home} path="/" exact />
+    <Route component={SignUp} path="/signup" />
+    <Route component={Spots} path="/spots" exact />
+    <Route component={Details} path="/spots/:id" />
   </BrowserRouter>
 );
 
