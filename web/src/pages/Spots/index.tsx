2d38b10790513b6988714a616577250c0bdfd216
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-07-08 09:41:08 -0300

    feat(Spots): create spot page

---
+++ web/src/pages/Spots/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Container, Header, Main, Footer,
+} from '@styles/Grid';
+
+import Navigator from '@components/Navigator';
+import Credits from '@components/Credits';
+import { Button } from '@components/Elements';
+
+import {
+  SpotList, Spot, SpotInfo,
+} from './styles';
+
+const Spots: React.FC = () => (
+  <Container flexDirection="column">
+    <Header>
+      <Navigator />
+    </Header>
+    <Main>
+      <SpotList>
+        <Spot>
+          <header styles={{ background-image: url('./assets/bkp.png'); }} />
+          <SpotInfo>
+            <strong>Test</strong>
+            <span>R$50/day</span>
+            <Button type="submit">BOOKING</Button>
+          </SpotInfo>
+        </Spot>
+        <Spot>
+          <header styles={{ background-image: url('./assets/bkp.png'); }} />
+          <SpotInfo>
+            <strong>Test</strong>
+            <span>R$50/day</span>
+            <Button type="submit">BOOKING</Button>
+          </SpotInfo>
+        </Spot>
+        <Spot>
+          <header styles={{ background-image: url('./assets/bkp.png'); }} />
+          <SpotInfo>
+            <strong>Test</strong>
+            <span>R$50/day</span>
+            <Button type="submit">BOOKING</Button>
+          </SpotInfo>
+        </Spot>
+        <Spot>
+          <header styles={{ background-image: url('./assets/bkp.png'); }} />
+          <SpotInfo>
+            <strong>Test</strong>
+            <span>R$50/day</span>
+            <Button type="submit">BOOKING</Button>
+          </SpotInfo>
+        </Spot>
+      </SpotList>
+    </Main>
+    <Footer>
+      <Credits />
+    </Footer>
+  </Container>
+);
+
+export default Spots;
