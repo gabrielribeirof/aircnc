44081f461762840e7560292db3ed8fee2aea8db5
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-07-08 09:23:17 -0300

    feat(App): implement auth provider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import { AuthProvider } from '@contexts/auth';
 import GlobalStyle from '@styles/global';
 import Routes from './routes';
 
 const App = () => (
-  <>
+  <AuthProvider>
     <Routes />
     <GlobalStyle />
-  </>
+  </AuthProvider>
 );
 
 export default App;
