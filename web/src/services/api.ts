34f17d60bd79ed3a164b54f65566361d2e250976
Gabriel Ribeiro <ogabrielribeirof@gmail.com>
2020-07-11 23:00:00 +0000

    feat(api): create auth axios instance

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://192.168.0.110:3333',
+  baseURL: process.env.REACT_APP_BACKEND,
+});
+
+export const auth = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem(('@Aircnc:token'))}`
+  }
 });
