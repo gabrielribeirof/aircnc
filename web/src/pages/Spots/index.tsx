7cb11303863c0fef7160a1b1408c9b181e726fa7
Gabriel Ribeiro <ogabrielribeirof@gmail.com>
2020-07-11 23:02:57 +0000

    feat(Spots): add spots search

--- web/src/pages/Spots/index.tsx
+++ web/src/pages/Spots/index.tsx
@@ -1,61 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { auth } from '@services/api';
+
 import {
   Container, Header, Main, Footer,
 } from '@styles/Grid';
-
 import Navigator from '@components/Navigator';
 import Credits from '@components/Credits';
 import { Button } from '@components/Elements';
-
 import {
   SpotList, Spot, SpotInfo,
 } from './styles';
 
-const Spots: React.FC = () => (
+interface ISpot {
+  id: string,
+  name: string,
+  price: string,
+  thumbnail: string,
+  tags: [string]
+}
+
+const Spots: React.FC = () => {
+  const [spots, setSpots] = useState<ISpot[]>([]);
+
+  useEffect(() => {
+    auth.get('spots').then((response) => {
+      setSpots(response.data);
+    });
+  }, []);
+
+  return (
   <Container flexDirection="column">
     <Header>
       <Navigator />
     </Header>
     <Main>
       <SpotList>
-        <Spot>
-          <header style={{ backgroundImage: `url(${'./assets/bkp.png'})` }} />
-          <SpotInfo>
-            <strong>Test</strong>
-            <span>R$50/day</span>
-            <Button type="submit">BOOKING</Button>
-          </SpotInfo>
-        </Spot>
-        <Spot>
-          <header style={{ backgroundImage: `url(${'./assets/bkp.png'})` }} />
-          <SpotInfo>
-            <strong>Test</strong>
-            <span>R$50/day</span>
-            <Button type="submit">BOOKING</Button>
-          </SpotInfo>
-        </Spot>
-        <Spot>
-          <header style={{ backgroundImage: `url(${'./assets/bkp.png'})` }} />
-          <SpotInfo>
-            <strong>Test</strong>
-            <span>R$50/day</span>
-            <Button type="submit">BOOKING</Button>
-          </SpotInfo>
-        </Spot>
-        <Spot>
-          <header style={{ backgroundImage: `url(${'./assets/bkp.png'})` }} />
-          <SpotInfo>
-            <strong>Test</strong>
-            <span>R$50/day</span>
-            <Button type="submit">BOOKING</Button>
-          </SpotInfo>
-        </Spot>
+        {spots.map((spot) => (
+          <Spot key={spot.id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail})` }} />
+            <SpotInfo>
+              <strong>{spot.name}</strong>
+              <span>${spot.price}/day</span>
+              <Button type="submit">BOOKING</Button>
+            </SpotInfo>
+          </Spot>
+        ))}
       </SpotList>
     </Main>
     <Footer>
       <Credits />
     </Footer>
   </Container>
-);
+  );
+};
 
 export default Spots;
