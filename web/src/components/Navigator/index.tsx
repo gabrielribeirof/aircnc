a939d2302271cbbefff16b8dc2d3a415a9c3b655
Gabriel Ribeiro <ogabrielribeirof@gmail.com>
2020-07-11 23:09:53 +0000

    feat(Navigator): switch buttons following login status

--- web/src/components/Navigator/index.tsx
+++ web/src/components/Navigator/index.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import { Col } from '@styles/Grid';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '@contexts/auth';
 
 import logo from '@assets/logo.png';
 
-import { LightButton } from '@components/Elements';
+import { Col } from '@styles/Grid';
+import { Button, LightButton } from '@components/Elements';
 import { Container, Logo, MenuLink } from './styles';
 
-const SearchHeader: React.FC = () => (
-  <Container>
-    <Col flex="1">
-      <Logo id="logo" src={logo} alt="Aircnc" />
-    </Col>
-    <Col justify="end" alignItems="center" flex="1">
-      <MenuLink to="/">Home</MenuLink>
-      <MenuLink to="/spots">Spots</MenuLink>
-      <MenuLink to="/" bold="true">Login</MenuLink>
-      <LightButton to="/signup">Sign Up</LightButton>
-    </Col>
-  </Container>
-);
+const SearchHeader: React.FC = () => {
+  const { signed } = useContext(AuthContext);
+
+  return (
+    <Container>
+      <Col flex="1">
+        <Logo id="logo" src={logo} alt="Aircnc" />
+      </Col>
+      <Col justify="end" alignItems="center" flex="1">
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="/spots">Spots</MenuLink>
+        {!signed ? (
+          <>
+            <MenuLink to="/" bold="true">Login</MenuLink>
+            <LightButton to="/signup">Sign Up</LightButton>
+          </>
+        ) : (
+          <Link to="/spots">
+            <Button>Create spot</Button>
+          </Link>
+        )}
+      </Col>
+    </Container>
+  )
+};
 
 export default SearchHeader;
