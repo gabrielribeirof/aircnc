80d998c945e5aaedf2d023b5bd2526de1f48b751
gabrielribeirof <ogabrielribeirof@gmail.com>
2020-11-29 18:26:02 -0300

    feat(backend/User): remove one-to-many relations

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs');
 
-const Spot = require('./Spot');
-const Booking = require('./Booking');
-
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,28 +17,11 @@ const UserSchema = new mongoose.Schema({
     required: true,
     select: false,
   },
-  spots: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Spot',
-    required: true,
-  }],
-  bookings: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Booking',
-    required: true,
-  }],
 });
 
 UserSchema.pre('save', async function (next) {
   const hash = await bcryptjs.hash(this.password, 10);
   this.password = hash;
-
-  next();
-});
-
-UserSchema.pre('remove', function (next) {
-  Spot.remove({ user: this._id });
-  Booking.remove({ user: this._id });
 
   next();
 });
