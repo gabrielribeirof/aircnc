92459a700adae51723227b577532a121e86184be
gabrielribeirof <ogabrielribeirof@gmail.com>
2020-12-09 00:27:31 -0300

    feat(web): replace core files to javascript

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
-import { AuthProvider } from '@contexts/auth';
-import GlobalStyle from '@styles/global';
+import { AuthProvider } from './contexts/auth';
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App = () => (
