3cbffba1d268ca0588607375e6e345550dbaf892
gabrielribeirof <ogabrielribeirof@gmail.com>
2020-12-09 00:34:10 -0300

    feat(web/styles): update global style

--- web/src/styles/global.js
+++ web/src/styles/global.js
@@ -2,25 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   :root {
-    --title: #222222;
-    --subtitle: #dddddd;
-    --subtitle-2: #999999;
-    --subtitle-3: #484848;
+    --primary: #444444;
+    --border: #dddddd;
+    --border-dark: #999999;
+    --border-darker: #484848;
 
-    --white: #ffffff;
-    --black: #000000;
-    --green: #008a05;
-    --yellow: #f9a839;
-    --blue: #008489;
     --salmon: #f05a5b;
+    --salmon-dark: #d94947;
   }
 
   * {
     box-sizing: border-box;
-  }
-
-  html, body, #root {
-    height: 100%;
+    outline: none;
   }
 
   body {
@@ -30,5 +23,4 @@ export default createGlobalStyle`
     text-rendering: optimizeLegibility;
     color: var(--title);
   }
-
 `;
