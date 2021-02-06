1ee578cb7148dffff73b8f6d4bd89c8ba3aacfc9
gabrielribeirof <ogabrielribeirof@gmail.com>
2021-02-05 23:56:05 -0300

    feat(web/routes): redeclarete routes with the new configuration

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route as RouteDOM, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
-import { useAuth } from '../contexts/auth';
+import Route from './Route';
 
-import Layout from '../components/Layout';
-import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import Spots from '../pages/Spots';
-import NewSpot from '../pages/Spots/New';
-import BookSpot from '../pages/Spots/Book';
-
-const Route = ({ component: Component, isPrivate = false, ...rest }) => {
-  const { user } = useAuth();
-
-  if (!user && isPrivate) {
-    return <Redirect to="/" />;
-  }
-
-  if (user && !isPrivate) {
-    return <Redirect to="/spots" />;
-  }
-
-  return <RouteDOM {...rest} render={(props) => <Component {...props} />} />;
-};
+import Spot from '../pages/Spot';
+import SpotList from '../pages/SpotList';
+import SpotForm from '../pages/SpotForm';
+import BookingForm from '../pages/BookingForm';
 
 const Routes = () => (
   <BrowserRouter>
-    <Layout>
-      <Route component={Home} path="/" exact />
+    <Switch>
+      {/* <Route component={Home} path="/" exact /> */}
+
+      <Route component={SignIn} path="/signin" />
       <Route component={SignUp} path="/signup" />
-      <Route component={Spots} path="/spots" exact isPrivate />
-      <Route component={NewSpot} path="/spots/new" isPrivate />
-      <Route component={BookSpot} path="/spots/:spot_id/book" isPrivate />
-    </Layout>
+
+      <Route component={Spot} path="/spot/:spot_id" isPrivate />
+      <Route component={SpotList} path="/spots" exact isPrivate />
+      <Route component={SpotForm} path="/spots/new" isPrivate />
+
+      <Route component={BookingForm} path="/book/:spot_id" isPrivate />
+    </Switch>
   </BrowserRouter>
 );
 
